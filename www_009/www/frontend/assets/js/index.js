39419002916812e25c482209cdd1dfe95bebcb7d
GersonESS <gebhsantos@gmail.com>
2025-02-18 13:11:57 -0300

    a

---
+++ www_009/www/frontend/assets/js/index.js
@@ -0,0 +1,20 @@
+// javascript do index.html
+
+window.onload = () => {
+    
+    get_username(1);
+
+}
+
+function get_username(id){
+
+    fetch(`http://localhost:3000/user/${id}`)
+    .then(response => {
+        if(response.status === 200){
+            console.log('ok');
+        } else {
+            console.log('nok');
+        }
+    })
+
+}
